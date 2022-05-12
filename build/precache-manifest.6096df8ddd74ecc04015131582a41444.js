afea5d0ed2432f5a43553fc54ed50ad8a5818e7b
priya2022 <glenraise@gmail.com>
2022-05-12 16:33:26 +0530

    up

---
+++ build/precache-manifest.6096df8ddd74ecc04015131582a41444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae8a52171167d614e42",
+    "url": "/static/css/main.b921f7ae.chunk.css"
+  },
+  {
+    "revision": "0ae8a52171167d614e42",
+    "url": "/static/js/main.0ae8a521.chunk.js"
+  },
+  {
+    "revision": "bb176e3d51737bf94cdc",
+    "url": "/static/js/1.bb176e3d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "049d68efe0bc877a35e5d2dca81b584c",
+    "url": "/index.html"
+  }
+];
